742b723237866a22e72b68f558f789555a483f99
Sijawusz Pur Rahnama <sija@sija.pl>
2020-12-28 22:17:23 +0100

    Add Backtracer.configure

--- src/backtracer.cr
+++ src/backtracer.cr
@@ -1,6 +1,10 @@
 module Backtracer
   class_getter(configuration) { Configuration.new }
 
+  def self.configure : Nil
+    yield configuration
+  end
+
   def self.parse(backtrace : Array(String) | String, **options) : Backtrace
     Backtrace::Parser.parse(backtrace, **options)
   end
