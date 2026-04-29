9fdc03370e844fbbb4c580ba65acd0b7e993878d
Sijawusz Pur Rahnama <sija@sija.pl>
2026-04-29 23:48:29 +0200

    Make `Configuration#src_path` use runtime pwd detection

--- src/backtracer/configuration.cr
+++ src/backtracer/configuration.cr
@@ -56,7 +56,11 @@ module Backtracer
     # Path considered as "root" of your project.
     #
     # See `Frame#under_src_path?`
-    property src_path : Path? = {{ Process::INITIAL_PWD }}.try { |path| Path[path] }
+    property src_path : Path? = begin
+      Path[Dir.current]
+    rescue File::NotFoundError
+      nil
+    end
 
     # Directories to be recognized as part of your app. e.g. if you
     # have an `engines` dir at the root of your project, you may want
